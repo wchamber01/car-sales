55fb16d1fb942dce2278ecb9908e9cf8a69f8c79
wchamber01 <williamchambers@gmail.com>
2019-12-11 19:55:37 -0500

    modified file structure and created reducer

---
+++ src/actions/index.js
@@ -0,0 +1,9 @@
+const removeFeature = item => {
+  return {};
+  // dispatch an action here to remove an item
+};
+
+const addFeature = item => {
+  return {};
+  // dipsatch an action here to add an item
+};
