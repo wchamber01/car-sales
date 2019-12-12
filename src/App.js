8edfc200a7036bf2acb785fff38d7139a5c399bb
wchamber01 <williamchambers@gmail.com>
2019-12-11 20:36:52 -0500

    adding connect to files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 import Header from "./components/Header";
 import AddedFeatures from "./components/AddedFeatures";
 import AdditionalFeatures from "./components/AdditionalFeatures";
@@ -9,12 +8,12 @@ function App() {
   return (
     <div className="boxes">
       <div className="box">
-        <Header car={state.car} />
-        <AddedFeatures car={state.car} />
+        <Header />
+        <AddedFeatures />
       </div>
       <div className="box">
-        <AdditionalFeatures additionalFeatures={state.additionalFeatures} />
-        <Total car={state.car} additionalPrice={state.additionalPrice} />
+        <AdditionalFeatures />
+        <Total />
       </div>
     </div>
   );
--- src/components/AddedFeatures.js
+++ src/components/AddedFeatures.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React from "react";
+import { connect } from "react-redux";
 
-import AddedFeature from './AddedFeature';
+import AddedFeature from "./AddedFeature";
 
 const AddedFeatures = props => {
   return (
@@ -19,4 +20,10 @@ const AddedFeatures = props => {
   );
 };
 
-export default AddedFeatures;
+const mapStateToProps = state => {
+  return {
+    car: state.car
+  };
+};
+
+export default connect(mapStateToProps)(AddedFeatures);
